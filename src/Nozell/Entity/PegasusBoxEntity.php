b391fdbbd9465527f065d580856b6e822edebdc9
Nozell <153051470+Imjustnozell@users.noreply.github.com>
2024-08-08 16:26:47 -0500

    Update PegasusBoxEntity.php

--- src/Nozell/Entity/PegasusBoxEntity.php
+++ src/Nozell/Entity/PegasusBoxEntity.php
@@ -88,7 +88,7 @@ class PegasusBoxEntity extends Living {
                 $playerName = $damager->getName();
 
                 // Comprobar cooldown
-                if (isset($this->cooldowns[$playerName]) && $currentTime < $this->cooldowns[$playerName] + 20) {
+                if (isset($this->cooldowns[$playerName]) && $currentTime < $this->cooldowns[$playerName] + 10) {
                     $damager->sendMessage("§cDebes esperar antes de volver a abrir la crate.");
                     return;
                 }
@@ -122,7 +122,7 @@ class PegasusBoxEntity extends Living {
                     }
                     if ($vote !== 0) {
                         Main::removeKeyBox($player, "pegasus", 1);
-                        Main::getInstance()->getCrateManager()->getRandomItemFromCrate("pegasus", $player->getName(), $this, "animation.dark_magma.opened", "controller.animation.dark_magma.opened");
+                        Main::getInstance()->getCrateManager()->getRandomItemFromCrate("pegasus", $player->getName(), $this);
                         $player->sendMessage("§aHas abierto una crate exitosamente!");
                     } else {
                         $player->sendMessage("§cAl parecer no tienes keys!");
